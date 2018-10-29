f4812054dc1c797037bb3ec40650ca6c3ad047b1
Madyan Eka <madyan.eka95@gmail.com>
2018-10-29 10:45:48 +0700

    registering a date filter

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@plugins/core-components'
+    '@plugins/core-components',
+    '@plugins/date-filter'
   ],
 
   /*
